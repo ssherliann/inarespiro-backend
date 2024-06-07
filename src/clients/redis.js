1248832e6100c31fc736d04823779e0ace73b1d6
ssherliann <popova.ann05@gmail.com>
2024-06-07 19:39:23 +0300

    update redis.js

--- src/clients/redis.js
+++ src/clients/redis.js
@@ -1,5 +1,11 @@
-const Redis = require("ioredis");
-const redis = new Redis();
+const Redis = require('ioredis');
 
-export default redis;
+const redis = new Redis({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || '',
+});
 
+redis.on('error', (err) => {
+    console.error('Redis error:', err);
+});
