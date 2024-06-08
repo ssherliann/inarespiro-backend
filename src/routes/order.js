611d25be4348dc42cca16b42bdeed3e96df16902
ssherliann <popova.ann05@gmail.com>
2024-06-09 01:18:48 +0300

    upd

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import Order from '../controllers/order/order';
 import authMiddleware from '../middlewares/authMiddleware';
+import { Create } from '../controllers/order/order'
 
 const router = express.Router();
 
-router.post('/', authMiddleware, Order.Create); 
+router.post('/', authMiddleware, Create); 
 router.get('/', authMiddleware, Order.List); 
 router.get('/my-orders', authMiddleware, Order.GetMyOrders); 
 
